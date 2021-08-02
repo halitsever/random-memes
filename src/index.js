4be6a4257c1dd64f7981e24c91275c4ba55366ba
halitsever <halitsever@protonmail.com>
2021-08-02 20:15:32 +0300

    registerCustomFont function added
    canvas's registerFont function added as registerCustomFont

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const { random } = require("./lib/random");
 const { fromReddit } = require("./lib/fromReddit");
 const { createMeme } = require("./lib/createMeme");
 const { createDuoMeme } = require("./lib/createDuoMeme");
+const { registerCustomFont } = require("./lib/registerCustomFont");
 
 module.exports = {
   fromReddit: fromReddit,
   random: random,
   createMeme: createMeme,
-  createDuoMeme: createDuoMeme
+  createDuoMeme: createDuoMeme,
+  registerCustomFont: registerCustomFont
 };
